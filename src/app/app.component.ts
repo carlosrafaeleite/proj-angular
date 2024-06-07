0bd1528f42d07ab67e6bd0ff224541b6a67662e6
carlosrafaeleite <crafaelrosa@gmail.com>
2024-06-07 13:44:23 -0300

    alteraçoes iniciais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,10 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+  <!--<router-outlet />-->
+  <h1>Iniciando Projeto</h1>
+  <h2>Testa ndo a primeira pagina</h2>
+  `,
 })
-export class AppComponent {
-  title = 'proj-angular';
-}
+export class AppComponent {}
